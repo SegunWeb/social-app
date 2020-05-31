faeefa4c2dfaf32f0c9c77b531733a70970113fc
SegunWeb <mrsegun2014@gmail.com>
2020-05-31 14:11:13 +0300

    add useEffect for simulation componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.js
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.js
@@ -1,25 +1,31 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 
 
 const ProfileStatusWithHooks = (props) =>  {
 
     const [editMode, setEditMode] = useState(false);
     const [editStatus, newStatus] = useState(props.status);
+
+    useEffect(() => {
+        newStatus(props.status)
+    }, [props.status]);
+
+
     const activateMode = () => {
           setEditMode(true)
     };
     const outMode = () => {
-        setEditMode(false)
+        setEditMode(false);
+        props.updateStatus(editStatus);
     };
     const onStatusChange = (e) => {
         newStatus(e.currentTarget.value);
-        props.updateStatus(editStatus);
     };
 
     return (
         <div>
             {!editMode &&
-                <div><span onDoubleClick={activateMode} >{props.status || "no status"} test</span></div>
+                <div><span onDoubleClick={activateMode} >{props.status || "no status"}</span></div>
             }
             {editMode &&
             <div>
