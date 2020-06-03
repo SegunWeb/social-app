03da0fed122369db96b090d7294c011e012fa3b9
SegunWeb <mrsegun2014@gmail.com>
2020-06-03 20:37:00 +0300

    added redux dev tools

--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -1,4 +1,4 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import profileReducer from "../reducer/ProfileReducer";
 import dialogsReducer from "../reducer/DialogsReducer";
 import sidebarReducer from "../reducer/sidebarReducer";
@@ -19,8 +19,9 @@ let rootReducer = combineReducers({
     form: formReducer,
     appInit: AppReducer
 });
-
-let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleware)));
+// let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 
 export default store;
