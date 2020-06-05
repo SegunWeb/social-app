f01bb5208cad8d543ab5f1de226a49df1a7bc0f2
SegunWeb <mrsegun2014@gmail.com>
2020-06-05 20:27:37 +0300

    Updates

---
+++ precache-manifest.0b44b781ca0f0f5693bc046ba5f3f03b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a1c8f747a034eb1a3447610bc103b7",
+    "url": "/social-app/index.html"
+  },
+  {
+    "revision": "8316e5da46cc22475e26",
+    "url": "/social-app/static/css/3.f2f2af58.chunk.css"
+  },
+  {
+    "revision": "807d18ff28fc4deec79e",
+    "url": "/social-app/static/css/4.bb5780fa.chunk.css"
+  },
+  {
+    "revision": "214c6da1f5a60caabef8",
+    "url": "/social-app/static/css/main.53f9b00b.chunk.css"
+  },
+  {
+    "revision": "6fb43ccaab5be19cdad2",
+    "url": "/social-app/static/js/2.26126bed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-app/static/js/2.26126bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8316e5da46cc22475e26",
+    "url": "/social-app/static/js/3.ee0609e2.chunk.js"
+  },
+  {
+    "revision": "807d18ff28fc4deec79e",
+    "url": "/social-app/static/js/4.716585c9.chunk.js"
+  },
+  {
+    "revision": "214c6da1f5a60caabef8",
+    "url": "/social-app/static/js/main.630e2d36.chunk.js"
+  },
+  {
+    "revision": "cd64d1d6151d1d4e81fe",
+    "url": "/social-app/static/js/runtime-main.489e9c3c.js"
+  },
+  {
+    "revision": "d28859c5f10d4f941941a8cc6592b2f0",
+    "url": "/social-app/static/media/myface.d28859c5.jpg"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/social-app/static/media/preloader.9064981b.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SegunWeb/social-app/precache-manifest.25f79e14484c4c00f581db0f8c328cb6.js"
+  "/social-app/precache-manifest.0b44b781ca0f0f5693bc046ba5f3f03b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SegunWeb/social-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/social-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
