f6b9f666bea15766f28b31011eb7cf57bdbbbe22
SegunWeb <mrsegun2014@gmail.com>
2020-06-05 20:16:25 +0300

    Updates

---
+++ precache-manifest.25f79e14484c4c00f581db0f8c328cb6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f86034ca48da55b28bdea7985eea0192",
+    "url": "/SegunWeb/social-app/index.html"
+  },
+  {
+    "revision": "8316e5da46cc22475e26",
+    "url": "/SegunWeb/social-app/static/css/3.f2f2af58.chunk.css"
+  },
+  {
+    "revision": "807d18ff28fc4deec79e",
+    "url": "/SegunWeb/social-app/static/css/4.bb5780fa.chunk.css"
+  },
+  {
+    "revision": "8230593e418e6af45782",
+    "url": "/SegunWeb/social-app/static/css/main.53f9b00b.chunk.css"
+  },
+  {
+    "revision": "6fb43ccaab5be19cdad2",
+    "url": "/SegunWeb/social-app/static/js/2.26126bed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SegunWeb/social-app/static/js/2.26126bed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8316e5da46cc22475e26",
+    "url": "/SegunWeb/social-app/static/js/3.ee0609e2.chunk.js"
+  },
+  {
+    "revision": "807d18ff28fc4deec79e",
+    "url": "/SegunWeb/social-app/static/js/4.716585c9.chunk.js"
+  },
+  {
+    "revision": "8230593e418e6af45782",
+    "url": "/SegunWeb/social-app/static/js/main.75bb9c7a.chunk.js"
+  },
+  {
+    "revision": "9c50cb013d714aefd1ca",
+    "url": "/SegunWeb/social-app/static/js/runtime-main.129c1291.js"
+  },
+  {
+    "revision": "d28859c5f10d4f941941a8cc6592b2f0",
+    "url": "/SegunWeb/social-app/static/media/myface.d28859c5.jpg"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/SegunWeb/social-app/static/media/preloader.9064981b.svg"
+  }
+]);
